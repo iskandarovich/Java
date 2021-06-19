0795ff3ed77ec0f202afa4f500536d8ab4a4123d
iskandarovich <tohirizzatov11@gmail.com>
2021-06-19 18:09:28 +0500

    js tayyor

---
+++ app.js
@@ -0,0 +1,112 @@
+console.log("1-Misol")
+var a = 7
+console.log("P=4*a")
+console.log(P=4*a)
+
+
+
+console.log("==========")
+
+
+
+console.log("2-Misol")
+var a = 5
+console.log("S=a*a")
+console.log(S=a*a)
+
+
+
+console.log("==========")
+
+
+
+console.log("3-Misol")
+var a = 8
+var b = 7
+console.log("S=a*b,P=2*(a+b)")
+console.log(S=a*b)
+console.log(P=2*(a+b))
+
+
+console.log("==========")
+
+
+
+console.log("4-Misol")
+var d = 6
+const pi=3.14
+console.log("L=pi*d,pi=3.14")
+console.log(L=pi*d)
+
+
+console.log("==========")
+
+
+
+console.log("5-Misol")
+var a = 33
+console.log("V=a*a*a,S=6*a*a")
+console.log(V=a*a*a)
+console.log(S=6*a*a)
+
+
+console.log("==========")
+
+
+
+console.log("6-Misol")
+var a = 5
+var b = 7
+var c = 2
+console.log("V=a*b*c,S=2*(a*b+b*c+a*c)")
+console.log(V=a*b*c)
+console.log(S=2*(a*b+b*c+a*c))
+
+console.log("==========")
+
+
+console.log("7-Misol")
+var R = 4
+console.log("L=2*pi*R,S=pi*R*R")
+console.log(L=2*pi*R)
+console.log(S=pi*R*R)
+
+
+console.log("==========")
+
+
+console.log("8-Misol")
+var a = 4
+var b = 10
+console.log("(a*b)/2")
+console.log((a*b)/2)
+
+console.log("==========")
+
+
+console.log("9-Misol")
+var a = 3
+var b = 4
+console.log("(a*b)**(1/2)")
+console.log((a*b)**(1/2))
+
+console.log("==========")
+
+
+console.log("10-Misol")
+var a = 4
+var b = 2
+console.log("a+b,a*b,a*a,b*b")
+console.log(a+b)
+console.log(a*b)
+console.log(a*a)
+console.log(b*b)
+
+console.log("11-Misol")
+var a = 4
+var b = 2
+console.log("a+b,a*b,a>0,b>0")
+console.log(a+b)
+console.log(a*b)
+console.log(a>0)
+console.log(b<0)
